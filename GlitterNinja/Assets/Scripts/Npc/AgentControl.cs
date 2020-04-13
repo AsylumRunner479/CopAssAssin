ce5d10ac496f97955fb4e3eb63a266b2153608b7
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-04-13 13:29:38 +1000

    go

---
+++ GlitterNinja/Assets/Scripts/Npc/AgentControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AgentControl : MonoBehaviour
+{
+    public Transform home, goal, follow;
+    public NavMeshAgent agent;
+    public GameObject[] SpawnPoints;
+    private int random;
+    public GameObject self;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        home.transform.position = new Vector3(Random.Range(-20f, 20f), 0, Random.Range(-20f, 20f));
+        SpawnPoints = GameObject.FindGameObjectsWithTag("Spawn");
+        random = Random.Range(0, SpawnPoints.Length);
+        
+        agent.transform.position = SpawnPoints[random].transform.position;
+        if (follow != null)
+        {
+            agent.SetDestination(follow.transform.position);
+        }
+        else
+        {
+            agent.SetDestination(home.position);
+            follow = SpawnManager.last;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.transform.position != follow.transform.position)
+        {
+            agent.SetDestination(home.position);
+            random = Random.Range(0, SpawnPoints.Length);
+            goal.transform.position = SpawnPoints[random].transform.position;
+        }
+        if (agent.transform.position == home.position)
+        {
+            SpawnManager.SpawnCount -= 1;
+            Destroy(self);
+            
+        }
+    }
+}
---
+++ GlitterNinja/Assets/Scripts/Npc/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static int SpawnCount;
+    public float timer = 0;
+    public GameObject agent;
+    public static Transform last;
+    public AgentControl control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime * 8;
+        if (timer <= 0 && SpawnCount <= 300)
+        {
+            if (SpawnCount >= 250)
+            {
+                control = last.GetComponent<AgentControl>();
+                control.follow = last;
+            }
+            
+            last = Instantiate(agent).transform;
+            
+            
+            SpawnCount += 1;
+            timer = 1;
+        }
+    }
+}
