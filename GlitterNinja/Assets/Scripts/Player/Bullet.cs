d47bd61f53326d06051a57b0317e610b1f324208
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-04-14 15:55:39 +1000

    added basic gun

---
+++ GlitterNinja/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject self;
+    public PlayerHandler player;
+    public Collider other;
+    public float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnColliderEnter(Collider collision)
+    {
+        PlayerHandler enemy = other.transform.GetComponent<PlayerHandler>();
+        
+        if (enemy != null)
+        {
+            enemy.DamagePlayer(damage);
+            Destroy(self);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
