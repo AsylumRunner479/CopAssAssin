cd19e2ba713a20658f3244e805216615c1993fe0
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-08-24 13:32:45 +1000

    added movement shoot

---
+++ GlitterNinja/Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCamera;
+    private void Start()
+    {
+        if (fpsCamera == null)
+        {
+            fpsCamera = GetComponent<Camera>();
+        }
+        if (fpsCamera == null)
+        {
+            fpsCamera = Camera.main;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position,
+            fpsCamera.transform.forward,
+            out hit,
+            range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
