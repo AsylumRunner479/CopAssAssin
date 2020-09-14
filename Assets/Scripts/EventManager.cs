d205d2b3c4a54937ff279df73d84359e1520a7b8
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-09-14 15:58:21 +1000

    changed version

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public delegate void OnGameStart();
+    public static event OnGameStart onGameStart;
+    float startTime = 3f;
+    private void Awake()
+    {
+        RegisteringToEvent();
+        StartCoroutine(GameStarts());
+    }
+    void RegisteringToEvent()
+    {
+        onGameStart += SpawnPlayer;
+        onGameStart += ResetScores;
+    }
+    void ResetScores()
+    {
+
+    }
+    void SpawnPlayer()
+    {
+
+    }
+    private void Start()
+    {
+        StartCoroutine(GameStarts());
+    }
+    IEnumerator GameStarts()
+    {
+        yield return new WaitForSeconds(startTime);
+        onGameStart();
+    }
+}
